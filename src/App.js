b3b593067b2970c504d189989f0bb62228a3f7bc
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-06-18 19:31:15 -0700

    putting list into the screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from "axios";
+import {useEffect, useState} from 'react';
+import Coin from './Coin';
+
 
 function App() {
+
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+
+  useEffect(()=>{
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=100&page=1&sparkline=false#')
+    .then(resp=>{setCoins(resp.data)})
+    .catch(err=>{console.log(err)});
+
+  }, []);
+
+  console.log(coins);
+
+  const handleChange = e =>{
+    setSearch(e.target.value);
+  }
+
+  const filterCoins =coins.filter(coin=>{
+    coin.name.toLowerCase().includes(search.toLowerCase());
+
+  });
+
+  
+
+
   return (
-    <div className="App">
-      
+    <div className="coin_app">
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input type="text" placeholder='Search' className='coin-input' onChange={handleChange}/>  
+        </form>
+      </div>
+      {coins.map(coin=>{
+        return(
+          <Coin 
+         
+          name={coin.name} 
+          img={coin.image} 
+          symbol={coin.symbol} 
+          price={coin.current_price}
+          volume = {coin.total_volume}
+         
+          ></Coin>
+        )
+      })}
     </div>
   );
 }
